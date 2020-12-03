a3a163934f566b2651b27e40b2d6c55e3d93b2c5
Uriel Aispuro <urielalexis64@gmail.com>
2020-12-02 19:30:41 -0700

    save function on submit form

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ListModel } from '../../models/list.model';
+import { ListsService } from '../../services/lists.service';
+import Swal from 'sweetalert2';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
+  list = new ListModel();
 
-  constructor() { }
+  constructor(private listsService: ListsService) {}
 
   ngOnInit(): void {
+    this.listsService.getLists().subscribe((resp) => {
+      console.log(resp);
+    });
   }
 
+  save(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+
+    Swal.fire({
+      title: 'Wait please',
+      text: 'Saving...',
+      icon: 'info',
+      allowOutsideClick: false,
+    });
+
+    Swal.showLoading();
+
+    let petition: Observable<any>;
+
+    if (this.list.id) {
+      petition = this.listsService.updateList(this.list);
+    } else {
+      petition = this.listsService.createList(this.list);
+    }
+    petition.subscribe((response) => {
+      Swal.fire({
+        title: 'Nice!',
+        text: `${this.list.name} was updated correctly`,
+        icon: 'success',
+      });
+    });
+  }
 }
