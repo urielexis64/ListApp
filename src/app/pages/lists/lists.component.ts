0f1a3d86f008295f96158a37a10d4d050468cb1d
Uriel Aispuro <urielalexis64@gmail.com>
2020-12-02 19:30:16 -0700

    Lists obtained OnInit function

--- src/app/pages/lists/lists.component.ts
+++ src/app/pages/lists/lists.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ListsService } from '../../services/lists.service';
+import { ListModel } from '../../models/list.model';
 
 @Component({
   selector: 'app-lists',
   templateUrl: './lists.component.html',
-  styleUrls: ['./lists.component.css']
+  styleUrls: ['./lists.component.css'],
 })
 export class ListsComponent implements OnInit {
-
-  constructor() { }
+  lists: ListModel[] = [];
+  constructor(private listsService: ListsService) {}
 
   ngOnInit(): void {
+    this.listsService
+      .getLists()
+      .subscribe((response) => (this.lists = response));
   }
-
 }
