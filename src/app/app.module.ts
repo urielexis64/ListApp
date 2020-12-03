ac93cb94121070287280ca3f60f9d479066f438b
Uriel Aispuro <urielalexis64@gmail.com>
2020-12-02 19:08:40 -0700

    Forms and HTTP modules imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './pages/list/list.component';
@@ -8,7 +10,7 @@ import { ListsComponent } from './pages/lists/lists.component';
 
 @NgModule({
   declarations: [AppComponent, ListComponent, ListsComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
